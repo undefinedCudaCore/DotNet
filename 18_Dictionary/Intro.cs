576bf5c19f56651bd6f4bb1678d6c5170b7c5145
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2024-02-22 22:02:47 +0200

    Dictionaries

---
+++ 18_Dictionary/Intro.cs
@@ -0,0 +1,35 @@
+﻿namespace _18_Dictionary
+{
+    internal class Intro
+    {
+        public static void DictionaryIntro()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>
+            {
+                { "Vilnius", 7 },
+                { "Kaunas", 6},
+                { "Siauliai", 8},
+                { "Jonava", 6 }
+            };
+
+            Dictionary<string, int> dict2 = new Dictionary<string, int>
+            {
+                { "Ieva", 7 },
+                { "Andrius", 6},
+                { "Siauliai", 8},
+                { "Jonava", 6 }
+            };
+
+            Dictionary<string, int> dict3 = new Dictionary<string, int>();
+            dict2.Add("Giedrius", 88);
+            foreach (var item in dict2)
+            {
+                //Console.WriteLine(item);
+                //Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+            Console.WriteLine(dict2["Siauliai"]);
+
+        }
+    }
+}
---
+++ 18_Dictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _18_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intro.DictionaryIntro();
+
+            // -------------------------- 01.1.
+            //TaskNo01.NamesAndAge();
+
+            // -------------------------- 01.2.
+            //Console.WriteLine("Please enter country: ");
+            //string enterValue = TaskNo01.EnterCountry();
+            //string capitalOfCountry = TaskNo01.CountryCapital(enterValue);
+            //Console.WriteLine(capitalOfCountry);
+
+            // -------------------------- 01.3.
+            //Console.WriteLine("Enter fruit title to add to list: ");
+            //string fruitTitle = Console.ReadLine();
+            //Console.WriteLine("Enter fruit quantity to add to list: ");
+            //string fruitQty = Console.ReadLine();
+
+            //Dictionary<string, int> returnedListOfFruitsToPrint =
+            //    TaskNo01.AddOrRenewFruitList(TaskNo01.FruitsTitleAndQty(), fruitTitle, fruitQty);
+
+            //Console.WriteLine();
+            //TaskNo01.PrintFruits(returnedListOfFruitsToPrint);
+
+            // -------------------------- 01.4.
+            Console.WriteLine("Enter student name: ");
+            string name = Console.ReadLine();
+
+            if (!String.IsNullOrEmpty(name))
+            {
+                Dictionary<string, List<int>> keyValuePairs = TaskNo01.GetStudentGrades(name);
+                TaskNo01.PrintGrade(keyValuePairs, name);
+            }
+
+
+        }
+    }
+}
