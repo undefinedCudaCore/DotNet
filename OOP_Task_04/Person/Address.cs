f25d6194cd069c581ef1a0ca26947a8713491c4b
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2024-03-03 18:25:13 +0200

    Add exercise 4.2.

---
+++ OOP_Task_04/Person/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace OOP_Task_04.Person
+{
+    internal class Address
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
--- OOP_Task_04/Program.cs
+++ OOP_Task_04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OOP_Task_04.Cars;
+using OOP_Task_04.Person;
 
 namespace OOP_Task_04
 {
@@ -6,24 +7,36 @@ namespace OOP_Task_04
     {
         static void Main(string[] args)
         {
-            Car car1 = new Car();
+            //Exercise 4.1.------------------------
+            //Car car1 = new Car();
 
-            car1.Make = "Dodge";
-            car1.Model = "Journey";
+            //car1.Make = "Dodge";
+            //car1.Model = "Journey";
 
 
-            Wheels continentalR16 = new Wheels("Continental", 16, 55, 205);
+            //Wheels continentalR16 = new Wheels("Continental", 16, 55, 205);
 
-            for (int i = 0; i < 4; i++)
-            {
-                car1.Wheels.Add(continentalR16);
-            }
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    car1.Wheels.Add(continentalR16);
+            //}
 
-            Car.RunEngine(car1);
+            //Car.RunEngine(car1);
 
-            ShowIsCarEngineOn(car1);
+            //ShowIsCarEngineOn(car1);
+
+            //Exercise 4.2.------------------------
+            Person1 person1 = new Person1();
+
+            person1.Name = "Test";
+            person1.Age = 30;
+            person1.MyAddress.City = "Kaunas";
+            person1.MyAddress.Street = "Kauno g. 12";
+
+            Console.WriteLine($"My name is {person1.Name}, I'm {person1.Age} years old. I am living in city called {person1.MyAddress.City} on {person1.MyAddress.Street} street.");
         }
 
+        //Exercise 4.1.------------------------
         public static void ShowIsCarEngineOn(Car car)
         {
             Console.WriteLine(car.Make);
