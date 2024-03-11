249b0d4897b58208c1e7f531af2e294d9a099137
undefinedCudaCore <teklithuania@gmail.com>
2024-03-11 14:56:29 +0200

    Accessibility Homework

---
+++ Accessibility_HomeWork/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace Accessibility_HomeWork
+{
+    internal class Employee
+    {
+        public string NameAndSurename { get; set; }
+        protected double _salary { get; set; }
+
+        public virtual double GetSalary()
+        {
+            return _salary;
+        }
+        public virtual double SetSalary(double salary)
+        {
+            _salary = salary;
+            return _salary;
+        }
+    }
+}
---
+++ Accessibility_HomeWork/Manager.cs
@@ -0,0 +1,24 @@
+﻿namespace Accessibility_HomeWork
+{
+    internal class Manager : Employee
+    {
+        public Manager(double salary)
+        {
+            Rand = new Random();
+            _salary = salary;
+        }
+
+        public double Bonus { get; set; }
+        public Random Rand { get; set; }
+
+        public sealed override double GetSalary()
+        {
+            Bonus = Rand.Next(50, 500);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Bonus to salsary: " + Bonus);
+            Console.WriteLine("-----------------");
+            _salary += Bonus;
+            return _salary;
+        }
+    }
+}
