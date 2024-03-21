edc06e4695048c673f73eb2d5b677d42224b3265
undefinedCudaCore <teklithuania@gmail.com>
2024-03-21 21:24:00 +0200

    Add anonimic methods exercises

---
+++ 10_1_Anonymous_functions/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _10_1_Anonymous_functions
+{
+    internal class Program
+    {
+        public delegate List<int> GetNumberList(List<int> list, int number2);
+        public delegate string MyGeneric<T>(T element);
+
+        static void Main(string[] args)
+        {
+
+            List<int> ints = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+            };
+
+            GetNumberList del = delegate (List<int> list, int number2)
+            {
+                List<int> result = new List<int>();
+                for (int i = 1; i < list.Count; i = i + number2)
+                {
+                    result.Add(list[i]);
+                }
+                return result;
+            };
+
+            foreach (var item in del(ints, 2))
+            {
+                Console.WriteLine(item);
+            }
+
+            MyGeneric<int> delType = delegate (int value)
+            {
+                return value.GetType().ToString();
+            };
+
+            Console.WriteLine(delType(5));
+        }
+    }
+}
---
+++ 10_delegates_intro/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _10_delegates_intro
+{
+    internal class Program
+    {
+        private delegate int NumberChanger(int number);
+        static void Main(string[] args)
+        {
+            //var numberDelegate1 = new NumberChanger(AddNumber);
+            //var numberDelegate2 = new NumberChanger(SubtractNumber);
+
+            //int additionalResult1 = numberDelegate1(5);
+            //int additionalResult2 = numberDelegate2(8);
+        }
+
+        public static int AddNumber(int number)
+        {
+            return 1 + number;
+        }
+
+        public static int SubtractNumber(int number)
+        {
+            return 1 - number;
+        }
+    }
+}
