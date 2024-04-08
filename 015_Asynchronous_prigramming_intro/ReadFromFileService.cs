6921d70021dd66ce7f7ba3f232484c2d8da7f0f5
undefinedCudaCore <teklithuania@gmail.com>
2024-04-08 20:36:40 +0300

    Add async await exerises No 1 and No 2

---
+++ 015_Asynchronous_prigramming_intro/ReadFromFileService.cs
@@ -0,0 +1,30 @@
+﻿namespace _015_Asynchronous_prigramming_intro
+{
+    internal class ReadFromFileService
+    {
+        private List<string> _list = new List<string>();
+        private int _count = 0;
+        private void GetFiles()
+        {
+            FileInfo[] cDirs = new DirectoryInfo(@"C:\Users\undefinedCudaCore\Desktop").GetFiles();
+
+            Console.Clear();
+
+            foreach (var dir in cDirs)
+            {
+                _list.Add(dir.FullName);
+                Console.WriteLine(dir);
+            }
+        }
+        public async Task PrintDesktopFiles(int howManyTimesDoAction)
+        {
+            while (_count < howManyTimesDoAction)
+            {
+                GetFiles();
+                await Task.Delay(5000);
+                _count++;
+            }
+        }
+
+    }
+}
