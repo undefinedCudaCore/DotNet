bd796244852430587257e2fffd7a4225231a3aac
undefinedCudaCore <teklithuania@gmail.com>
2024-04-08 19:19:22 +0300

    add Asynchronous_prigramming_intro

---
+++ 015_Asynchronous_prigramming_intro/Intro.cs
@@ -0,0 +1,12 @@
+﻿namespace _015_Asynchronous_prigramming_intro
+{
+    internal static class Intro
+    {
+        public static async Task ProcessOrederAsync(string orderName, int delay)
+        {
+            Console.WriteLine($"{orderName} processing started.");
+            await Task.Delay(delay);
+            Console.WriteLine($"{orderName} processed.");
+        }
+    }
+}
---
+++ 015_Asynchronous_prigramming_intro/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _015_Asynchronous_prigramming_intro
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //First example
+            Console.WriteLine("Order processing started");
+            await Intro.ProcessOrederAsync("Order 1", 2000);
+            await Intro.ProcessOrederAsync("Order 2", 1000);
+            Console.WriteLine("All orders processed.");
+        }
+    }
+}
